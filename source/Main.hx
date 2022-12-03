fccf502cdfb74579671a3006eb033b5fe0f66f74
Mateus Soares <105178173+MateuzinhoX02@users.noreply.github.com>
2022-12-03 14:19:41 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -77,9 +77,7 @@ class Main extends Sprite
 
 		
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
-		
-		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
-		
+
 		addChild(game);
 		
 		#if android
